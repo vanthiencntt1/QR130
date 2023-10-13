170178767cfdd59c474e4fd37b41399e895f4a05
vanthiencntt1 <vanthiencntt@gmail.com>
2023-10-13 13:38:18 +0700

    add code

---
+++ DM/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace DM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtMa = new System.Windows.Forms.TextBox();
+            this.txtHoTen = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.txtDeleteId = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get ALL";
+            this.button1.UseVisualStyleBackColor = true;
+          //  this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 179);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(762, 247);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(36, 73);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "GetByID";
+            this.button2.UseVisualStyleBackColor = true;
+         //   this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtid
+            // 
+            this.txtid.Location = new System.Drawing.Point(36, 114);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(75, 20);
+            this.txtid.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(388, 27);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Insert(Post)";
+            this.button3.UseVisualStyleBackColor = true;
+          //  this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txtMa
+            // 
+            this.txtMa.Location = new System.Drawing.Point(247, 30);
+            this.txtMa.Name = "txtMa";
+            this.txtMa.Size = new System.Drawing.Size(100, 20);
+            this.txtMa.TabIndex = 6;
+            // 
+            // txtHoTen
+            // 
+            this.txtHoTen.Location = new System.Drawing.Point(247, 76);
+            this.txtHoTen.Name = "txtHoTen";
+            this.txtHoTen.Size = new System.Drawing.Size(100, 20);
+            this.txtHoTen.TabIndex = 7;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(650, 27);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "DeleteID";
+            this.button4.UseVisualStyleBackColor = true;
+          //  this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // txtDeleteId
+            // 
+            this.txtDeleteId.Location = new System.Drawing.Point(532, 29);
+            this.txtDeleteId.Name = "txtDeleteId";
+            this.txtDeleteId.Size = new System.Drawing.Size(100, 20);
+            this.txtDeleteId.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtDeleteId);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.txtHoTen);
+            this.Controls.Add(this.txtMa);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.txtid);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox txtMa;
+        private System.Windows.Forms.TextBox txtHoTen;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox txtDeleteId;
+    }
+}
+
---
+++ DM/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+
+namespace DM
+{
+    public class dmbs
+    {
+        public string ID { get; set; }
+    }
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public static readonly string baseURL = "http://localhost:8083/Dmbs";
+        // Get All
+        public static async Task<string> GetALL()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage res = await client.GetAsync(baseURL + "/Dm"))
+                {
+                    using (HttpContent content = res.Content)
+                    {
+                        string data = await content.ReadAsStringAsync();
+                        return data;
+                    }
+                }
+            }
+
+        }
+
+        // Get By ID 
+
+        public static async Task<string> GetID(string Id)
+        {
+            using (HttpClient client = new HttpClient()) //DmbyID? Id = 0014
+            {
+                using (HttpResponseMessage res = await client.GetAsync(baseURL + "/DmbyID?ID=" + Id + ""))
+                {
+                    using (HttpContent content = res.Content)
+                    {
+                        string data = await content.ReadAsStringAsync();
+                        return data;
+                    }
+                }
+            }
+
+        }
+        // Post
+        public static async Task<string> PosstID(string Id, string hoten)
+        {
+            string inputdata = $"{{ \"MA\":\"{Id}\", \"HOTEN\":\"{hoten}\" }}";
+            StringContent input = new StringContent(inputdata, Encoding.UTF8, "application/json");
+
+
+            using (HttpClient client = new HttpClient()) //DmbyID? Id = 0014
+            {
+                using (HttpResponseMessage res = await client.PostAsync(baseURL + "/PostBS", input))
+                {
+                    using (HttpContent content = res.Content)
+                    {
+                        string data = await content.ReadAsStringAsync();
+                        return data;
+                    }
+                }
+            }
+
+        }
+
+        //DeleteID
+        //public static async Task<string> DeleteID(string Id)
+        //{
+        //    string inputdata = $"{{ \"MA\":\"{Id}\"}}";
+        //     StringContent input = new StringContent(inputdata, Encoding.UTF8, "application/json");
+        //    using (HttpClient client = new HttpClient()) //DmbyID? Id = 0014
+        //    {
+
+        //        using (HttpResponseMessage res = await client.DeleteAsync(baseURL + "/Delete"))
+        //        {
+        //            using (HttpContent content = res.Content)
+        //            {
+        //                string data = await content.ReadAsStringAsync();
+        //                return data;
+        //            }
+        //        }
+        //    }
+
+        //}
+
+
+        public async Task<string> DeleteData(string MA)
+        {
+            HttpClient client = new HttpClient();
+            try
+            {
+                string inputdata = $"{{ \"MA\":\"{MA}\" }}";
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Delete,
+                    RequestUri = new Uri(baseURL + "/Delete"),
+                    // doi sang kiểu json  JsonConvert.SerializeObject(inputdata)
+                    //   Content = new StringContent(JsonConvert.SerializeObject(inputdata), Encoding.UTF8, "application/json")
+                    Content = new StringContent(inputdata, Encoding.UTF8, "application/json")
+                };
+
+                using (HttpResponseMessage response = await client.SendAsync(request))
+
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(responseBody);
+                    response.EnsureSuccessStatusCode(); // Kiểm tra xem có lỗi HTTP không
+
+                    string responseData = await response.Content.ReadAsStringAsync();
+                    return responseData;
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                // Xử lý các lỗi HTTP, mạng và khác nhau có thể xảy ra ở đây
+                Console.WriteLine($"Lỗi HTTP: {ex.Message}");
+                throw;
+            }
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var responce = await GetALL();
+            //  dataGridView1 =responce;
+            richTextBox1.Text = responce.ToString();
+
+
+            //Tải dữ liệu lên dataGridView
+            //   dataGridView1.DataSource = responce;
+            ;
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+
+        {
+            string text = txtid.Text.Trim();
+            if (String.IsNullOrEmpty(text))
+
+            {
+
+                MessageBox.Show("Vui lòng nhập Id", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+
+                var responce = await GetID(text);
+                //  dataGridView1 =responce;
+                richTextBox1.Text = responce.ToString();
+            }
+
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            string textma = txtMa.Text.Trim();
+            string texthoten = txtHoTen.Text.Trim();
+            if (String.IsNullOrEmpty(textma) || String.IsNullOrEmpty(textma))
+
+            {
+
+                MessageBox.Show("Vui lòng nhập thông tin", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+
+                var responce = await PosstID(textma, texthoten);
+                //  dataGridView1 =responce;
+                richTextBox1.Text = responce.ToString();
+            }
+
+        }
+
+        private async void button4_Click(object sender, EventArgs e)
+        {
+
+            {
+                string text = txtDeleteId.Text.Trim();
+                if (String.IsNullOrEmpty(text))
+
+                {
+
+                    MessageBox.Show("Vui lòng nhập Mã bs", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+
+                    var responce = await DeleteData(text);
+                    //  dataGridView1 =responseData;
+                    richTextBox1.Text = responce.ToString();
+
+
+                }
+
+            }
+        }
+
+
+
+
+
+    }
+}
